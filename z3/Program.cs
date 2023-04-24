e0ec5889b699b65d9c0124f988517fad8c42aefb
Stas <staspolevechko@gmail.com>
2023-04-24 22:54:13 +0300

    задача 3 готова

--- z3/Program.cs
+++ z3/Program.cs
@@ -4,3 +4,39 @@
 7 -> да
 1 -> нет
 */
+int promrt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine() ??"";
+    int result = Convert.ToInt32(value);
+    return result;
+}
+bool IsWeekend(int weekDay)
+{
+    if (weekDay > 5)
+    {
+        return true;
+    }
+    return false;
+}
+bool ValidateWeekDay(int number)
+{
+    if (number >0 && number <=7)
+    {
+        return true;
+    }
+    Console.WriteLine("это не день недели");
+    return false;
+}
+int weekDay =promrt("введите день недели: ");
+if (ValidateWeekDay(weekDay))
+{
+    if (IsWeekend(weekDay))
+    {
+        Console.WriteLine("наконецто выходной");
+    }
+    else
+    {
+        Console.WriteLine("придется идти на работу");
+    }
+}
